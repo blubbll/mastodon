69f716d0441165cec56d8af731dd245a5e68799f
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2018-11-11 18:05:08 +0100

    seperating tools, + login via quickKey

--- masto-auth.js
+++ masto-auth.js
@@ -1,11 +1,7 @@
-//Login route
-app.post('/m-login', urlencodedParser, function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var twoFa = req.body.twofa;
-    var newKey = req.body.newKey;
+//tools for mastoAuth
+const mAuth = {
     //normal login check
-    const checkLogin = (html) => {
+    checkLogin: (html) => {
         const $ = cheerio.load(html);
         //get token
         var msg = $(".alert.flash-message").text();
@@ -24,16 +20,16 @@ app.post('/m-login', urlencodedParser, function(req, res) {
                 success: false,
                 message: msg
             };
-        } else if (twoFa === '') {
+        } else if (mAuth.twoFa === '') {
             return {
                 success: false,
                 twofa: true,
                 message: $(".hint").text()
             };
         }
-    }
+    },
     //2fa check
-    const checkLoginTwo = (html) => {
+    checkLoginTwo: (html) => {
         const $ = cheerio.load(html);
         //get token
         var msg = $(".alert.flash-message").text();
@@ -52,9 +48,9 @@ app.post('/m-login', urlencodedParser, function(req, res) {
                 message: msg
             };
         }
-    };
-    //extract masterkey for API
-    const getAppInfo = async (browser) => {
+    },
+    //extract mappInfo to get masterKey
+    getAppInfo: async (browser) => {
         await browser.visit(`${host}/settings/applications`);
         let $ = cheerio.load(browser.document.documentElement.innerHTML);
         const appInfo = {
@@ -88,11 +84,11 @@ app.post('/m-login', urlencodedParser, function(req, res) {
             });
         }
         return appInfo;
-    };
+    },
     //get masterKey from app
-    const getMasterKey = async (browser) => {
+    getMasterKey: async (browser) => {
         let key; //do something with key, like store in db?
-        const appInfo = await getAppInfo(browser);
+        const appInfo = await mAuth.getAppInfo(browser);
         //was newly created? get key!
         if (appInfo.wasNew) {
             //go to app
@@ -113,63 +109,82 @@ app.post('/m-login', urlencodedParser, function(req, res) {
             key = $($(".table-wrapper>.table code")[2]).text();
         }
         return key;
-    };
+    },
+    //get masterKey from app
+    refreshMasterKey: async (browser) => {
+        browser.clickLink("a.table-action-link");
+        await browser.wait();
+    }
+}
+//login route
+app.post('/m-login', urlencodedParser, async (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    var twoFa = req.body.twofa;
+    var key = req.body.key;
+    var newKey = req.body.newKey;
     //create browser for headless requests
-    (async () => {
-        //create browser for headless requests
-        const browser = new Browser(zombieOptions); //https://stackoverflow.com/a/12105401
-        if (req.headers['accept-language'] !== undefined) browser.headers['accept-language'] = req.headers['accept-language'];
-        browser.visit(`${host}/auth/sign_in`);
-        await browser.wait();
-        //fill data
-        browser.fill('input#user_email', email);
-        browser.fill('input#user_password', password);
-        //submit form
-        browser.document.forms[0].submit();
-        await browser.wait();
-        let html = browser.document.documentElement.innerHTML;
-        let result = await checkLogin(html);
-        html = browser.document.documentElement.innerHTML; // get new html (after result)
-        const doLogout = async () => {
-            browser.clickLink("a[data-method=delete]");
+
+    const check = await (fetch(`${host}/api/v1/accounts/verify_credentials?access_token=${key}`));
+    if ((+(await (check.status)) === 200)) {
+        res.status(200).json({
+            key: key
+        });
+        logger.info(`\n\t${email} logged in using quickKey\t`);
+    } else
+        (async () => {
+            //create browser for headless requests
+            const browser = new Browser(zombieOptions); //https://stackoverflow.com/a/12105401
+            if (req.headers['accept-language'] !== undefined) browser.headers['accept-language'] = req.headers['accept-language'];
+            browser.visit(`${host}/auth/sign_in`);
             await browser.wait();
-            logger.info(`\n\tended auth session for ${email}.\t`);
-            [browser.tabs.closeAll(), browser.destroy()];
-        }
-        const returnSuccess = async (result) => {
-            //user requested key
-            if (newKey) res.status(200).json({
-                id: result.id,
-                key: await getMasterKey(browser)
-            });
-            //normal answer
-            else res.status(200).json({
-                id: result.id,
-            });
-        }
-        if (result !== undefined && result.success) {
-            
-            
-            await returnSuccess(result);
-            await doLogout();
-        } else if (twoFa !== '' && html.includes('id="user_otp_attempt"')) {
-            browser.fill('input#user_otp_attempt', twoFa);
+            //fill data
+            browser.fill('input#user_email', email);
+            browser.fill('input#user_password', password);
+            //submit form
             browser.document.forms[0].submit();
             await browser.wait();
-            let result = await checkLoginTwo(browser.document.documentElement.innerHTML);
-            if (result.success) {
+            let html = browser.document.documentElement.innerHTML;
+            let result = await mAuth.checkLogin(html);
+            html = browser.document.documentElement.innerHTML; // get new html (after result)
+            const doLogout = async () => {
+                browser.clickLink("a[data-method=delete]");
+                await browser.wait();
+                logger.info(`\n\tended auth session for ${email}.\t`);
+                [browser.tabs.closeAll(), browser.destroy()];
+            }
+            const returnSuccess = async (result) => {
+                //user requested key
+                if (newKey) res.status(200).json({
+                    id: result.id,
+                    key: await mAuth.getMasterKey(browser)
+                });
+                //normal answer
+                else res.status(200).json({
+                    key: key
+                });
+            }
+            if (result !== undefined && result.success) {
                 await returnSuccess(result);
                 await doLogout();
-            }
-        } else if (!result.success && result.twofa)
-            res.status(400).json({
-                twofa: true,
+            } else if (twoFa !== '' && html.includes('id="user_otp_attempt"')) {
+                browser.fill('input#user_otp_attempt', twoFa);
+                browser.document.forms[0].submit();
+                await browser.wait();
+                let result = await mAuth.checkLoginTwo(browser.document.documentElement.innerHTML);
+                if (result.success) {
+                    await returnSuccess(result);
+                    await doLogout();
+                }
+            } else if (!result.success && result.twofa)
+                res.status(400).json({
+                    twofa: true,
+                    msg: result.message
+                });
+            else res.status(500).json({
                 msg: result.message
             });
-        else res.status(500).json({
-            msg: result.message
-        });
-    })();
+        })();
 });
 //Signup route
 app.post('/m-register', urlencodedParser, async function(req, res) {
@@ -180,7 +195,6 @@ app.post('/m-register', urlencodedParser, async function(req, res) {
         const $ = cheerio.load(html);
         //extract entered id
         let id = $('input#user_account_attributes_username').val();
-        //get token
         var msg = $(".error").text();
         let success = $("#new_user")[0] !== undefined && $("#error_explanation").text() === '';
         if (success) {
@@ -261,5 +275,5 @@ app.post('/m-reset', urlencodedParser, async function(req, res) {
     browser.wait().then(function() {
         const $ = cheerio.load(browser.document.documentElement.innerHTML);
         res.status(200).json($(".flash-message.notice").text());
-    })
+    });
 });
